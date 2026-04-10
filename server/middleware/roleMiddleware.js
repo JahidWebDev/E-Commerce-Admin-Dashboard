f365ddb0a64875c2591f536943c084af617c2b43
JahidWebDev <webdeveloper2324@gmail.com>
2026-04-10 23:38:39 +0600

    update

---
+++ server/middleware/roleMiddleware.js
@@ -0,0 +1,10 @@
+function roleMiddleware(requiredRole) {
+    return( req, res, next) => {
+        if (!req.session.isAuth) {
+            return res.json({ error: "Unauthorized user" });
+        }
+        if (!req.session.user.role !== requiredRole) {
+            return res.json({ error: "Unauthorized user"})
+        }
+    }
+}
